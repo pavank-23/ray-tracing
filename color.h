75590d862847cbd86ab94d879c1493ed3c0d3e10
Pavan K <23pavank@gmail.com>
2024-09-29 18:16:43 +0530

    Hittable objects abstraction and formatting fix

--- color.h
+++ color.h
@@ -7,7 +7,8 @@
 
 using color = vec3;
 
-void write_color(std::ostream& out, const color& pixel_color) {
+void write_color(std::ostream &out, const color &pixel_color)
+{
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
