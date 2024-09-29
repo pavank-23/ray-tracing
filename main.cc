ba50a85a9ba856007a0087d2540cefde1c0b0d3e
Pavan K <23pavank@gmail.com>
2024-09-29 18:09:46 +0530

    Shading with Shader Normals

--- main.cc
+++ main.cc
@@ -3,24 +3,38 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3 &center, double radius, const ray &r)
+{
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
-color ray_color(const ray& r) {   
-     if (hit_sphere(point3(0,0,-1), 0.5, r))
-        return color(1, 0, 0);
+color ray_color(const ray &r)
+{
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0)
+    {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0); // linear interpolation between blue and white
 }
 
-int main() {
+int main()
+{
     // Image
     auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
@@ -30,9 +44,8 @@ int main() {
     // Camera
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto viewport_width = viewport_height * (double(image_width) / image_height);
     auto camera_center = point3(0, 0, 0);
-
 
     // Calculate the vector across the horizontal and down the viewport
     auto viewport_u = vec3(viewport_width, 0, 0);
@@ -48,11 +61,14 @@ int main() {
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n"
+              << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
+    for (int j = 0; j < image_height; j++)
+    {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) {
+        for (int i = 0; i < image_width; i++)
+        {
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
             auto ray_direction = pixel_center - camera_center;
             ray r(camera_center, ray_direction);
